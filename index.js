85b75ff84ca707d76893f26b1601244608d1bc49
Andrea M <andrea.ramirez@andrea.ramirez-MacBook-Pro>
2024-10-30 15:07:41 -0700

    finished w/o extension part

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const keyword = 'dogs';
+const key = process.env.API_KEY;
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${keyword}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+
+async function getGiph() {
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        const singleGiph = data.data[0];
+        console.log(singleGiph);
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+getGiph();
